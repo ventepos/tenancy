7d59c7cd248c580c68f98e09c16ae2d7fcb31168
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-06-20 11:13:37 +0200

    Also make a MySQL one

---
+++ tests/unit/Database/Mysql/MysqlDriverTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Tests\Database;
+
+use Tenancy\Testing\DatabaseDriverTestCase;
+use Tenancy\Database\Drivers\Mysql\Providers\ServiceProvider;
+
+class MysqlDriverTest extends DatabaseDriverTestCase
+{
+    protected $additionalProviders = [ServiceProvider::class];
+}
