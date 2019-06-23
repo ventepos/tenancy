e77cf9262fbccfafe872af389ba774777070eadb
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-06-23 13:32:46 +0200

    Change Configuring funcionality

--- src/Tenancy/Database/Events/Drivers/Configuring.php
+++ src/Tenancy/Database/Events/Drivers/Configuring.php
@@ -53,13 +53,25 @@ class Configuring
         return $this;
     }
 
-    public function useConfig(string $path)
+    public function useConfig(string $path, array $override = [])
     {
         if (! file_exists($path)) {
             throw new InvalidArgumentException("Cannot set up tenant connection configuration, file $path does not exist.");
         }
 
-        $this->configuration = include $path;
+        $this->configuration = array_merge(
+            include $path,
+            $override
+        );
+
+        return $this;
+    }
+
+    public function useArray(array $array, array $override = []){
+        $this->configuration = array_merge(
+            $array,
+            $override
+        );
 
         return $this;
     }
